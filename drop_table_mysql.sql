b706dcb6457522ea3d112dda4a2b3319558e96f0
Dawid Nowotny <93731073+ayaseshi@users.noreply.github.com>
2022-01-04 11:49:52 +0100

    Add Drop Table MYSQL
    Dodano zapytania usuwające bazę danych mysql na potrzeby interfejsu graficznego.

---
+++ drop_table_mysql.sql
@@ -0,0 +1,15 @@
+ALTER TABLE informacje_osobowe DROP FOREIGN KEY ifad_fk;
+ALTER TABLE kontrole DROP FOREIGN KEY kp_fk;
+ALTER TABLE kontrole DROP FOREIGN KEY ks_fk;
+ALTER TABLE placowki DROP FOREIGN KEY pa_fk;
+ALTER TABLE pracownicy DROP FOREIGN KEY pif_fk;	
+ALTER TABLE pracownicy DROP FOREIGN KEY ppl_fk;
+ALTER TABLE pracownicy DROP FOREIGN KEY pst_fk;
+ALTER TABLE samochody DROP FOREIGN KEY sdp_fk;
+ALTER TABLE samochody DROP FOREIGN KEY spp_fk;
+ALTER TABLE samochody DROP FOREIGN KEY ssp_fk;
+ALTER TABLE samochody DROP FOREIGN KEY sw_fk;
+ALTER TABLE wlasciciele DROP FOREIGN KEY wif_fk;	
+ALTER TABLE zmiany_pracownicze DROP FOREIGN KEY zpp_fk;	
+DROP TABLE IF EXISTS
+kontrole, pracownicy, samochody, placowki, zmiany_pracownicze, stanowiska, dane_pojazdow, stany_pojazdow, produkcja_pojazdow, wlasciciele, informacje_osobowe, adresy CASCADE;
