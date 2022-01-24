323ceba21c879d6a3377173e821d18e4671613fa
Radosław Medalion <96257825+Radoslaw2000@users.noreply.github.com>
2022-01-24 20:29:33 +0100

    Update Create View Oracle
    Aktualizacja zapytań tworzących widoki w Oracle.

--- create_view_oracle.sql
+++ create_view_oracle.sql
@@ -3,23 +3,17 @@ create or replace view view_wlasciciele (id,imie,nazwisko,plec,data_urodzenia,pe
 	as (SELECT w.w_id, i.if_imie, i.if_nazwisko, i.if_plec, i.if_data_urodzenia, i.if_pesel, i.if_telefon, a.ad_kraj, a.ad_wojewodztwo, a.ad_miasto, a.ad_ulica, a.ad_nr_domu, a.ad_kod_pocztowy, 
 	w.w_data_prawa_jazdy, w.w_waznosc_prawa_jazdy, w.w_ilosc_samochodow, w.w_procent_znizek_oc, i.ad_id, w.if_id  FROM wlasciciele w, informacje_osobowe i, adresy a WHERE w.if_id = i.if_id AND i.ad_id = a.ad_id);
 select * from view_wlasciciele;
-drop view view_wlasciciele;
-
 
 create or replace view view_placowki (id,kraj,wojewodztwo,miasto,ulica,nr_domu,kod_pocztowy, telefon, fax, godzina_otwarcia, godzina_zamkniecia, ad_id) 
 	as (SELECT pl.pl_id, a.ad_kraj, a.ad_wojewodztwo, a.ad_miasto, a.ad_ulica, a.ad_nr_domu, a.ad_kod_pocztowy, pl.pl_telefon, pl.pl_fax, pl.pl_godzina_otwarcia, pl.pl_godzina_zamkniecia, pl.ad_id
 	FROM placowki pl, adresy a WHERE pl.ad_id = a.ad_id);
 select * from view_placowki;
-drop view view_placowki;
-
 
 create or replace view view_pracownicy (id,imie,nazwisko,plec,data_urodzenia,pesel,telefon,kraj,wojewodztwo,miasto,ulica,nr_domu,kod_pocztowy, przepracowane_godziny, stawka, nazwa_stanowiska, opis_stanowiska, premia, st_id, ad_id, if_id) 
 	as (SELECT p.p_id, i.if_imie, i.if_nazwisko, i.if_plec, i.if_data_urodzenia, i.if_pesel, i.if_telefon, a.ad_kraj, a.ad_wojewodztwo, 
 	a.ad_miasto, a.ad_ulica, a.ad_nr_domu, a.ad_kod_pocztowy, p.p_przepracowane_godziny, p.p_stawka, st.st_nazwa, st.st_opis, st.st_premia, p.st_id, i.ad_id, p.if_id  
 	FROM pracownicy p, stanowiska st, informacje_osobowe i, adresy a WHERE p.if_id = i.if_id AND i.ad_id = a.ad_id AND p.st_id = st.st_id);
 select * from view_pracownicy;
-drop view view_pracownicy;
-
 
 create or replace view view_samochody (id, numer_rejestracyjny, marka, model, generacja, typ, przebieg, paliwo, pojemnosc, 
 	moc_km, moc_kw, masa_wlasna, kraj_produkcji, data_produkcji, data_pierwszej_rejestracji, data_rejestracji_w_kraju, ilosc_wlascicieli) 
@@ -27,8 +21,6 @@ create or replace view view_samochody (id, numer_rejestracyjny, marka, model, ge
 	dp.dp_moc_km, dp.dp_moc_kw, dp.dp_masa_wlasna, pp.pp_kraj_produkcji, pp.pp_data_produkcji, pp.pp_data_pierwszej_rejestracji, pp.pp_data_rejestracji_w_kraju, pp.pp_ilosc_wlascicieli
 	FROM samochody s, dane_pojazdow dp, produkcja_pojazdow pp WHERE s.dp_id = dp.dp_id AND s.pp_id = pp.pp_id );
 select * from view_samochody;
-drop view view_samochody;
-
 
 create or replace view view_kontrole (id, numer_rejestracyjny, marka, imie_wlasciciela, nazwisko_wlasciciela, przebieg, wynik, lpg, hak, data_kontroli, data_nastepnej_kontroli, cena, stan_silnika, stan_hamulcy,
 	stan_amortyzatorow, stan_zawieszenia, stan_zarowek, stan_lamp) 
@@ -36,8 +28,6 @@ create or replace view view_kontrole (id, numer_rejestracyjny, marka, imie_wlasc
 	sp.sp_amortyzatory, sp.sp_zawieszenie, sp.sp_zarowki, sp.sp_lampy
 	FROM kontrole k, samochody s, dane_pojazdow dp, stany_pojazdow sp, wlasciciele w, informacje_osobowe i WHERE k.s_id = s.s_id AND s.dp_id = dp.dp_id AND s.sp_id = sp.sp_id AND s.w_id = w.w_id AND w.if_id = i.if_id );
 select * from view_kontrole;
-drop view view_kontrole;
-
 
 create or replace view view_kontrole_week (id, numer_rejestracyjny, marka, imie_wlasciciela, nazwisko_wlasciciela, przebieg, wynik, lpg, hak, 
     data_kontroli, data_nastepnej_kontroli, cena, stan_silnika, stan_hamulcy, stan_amortyzatorow, stan_zawieszenia, stan_zarowek, stan_lamp) 
@@ -46,12 +36,9 @@ create or replace view view_kontrole_week (id, numer_rejestracyjny, marka, imie_
 	FROM kontrole k, samochody s, dane_pojazdow dp, stany_pojazdow sp, wlasciciele w, informacje_osobowe i 
     WHERE k.s_id = s.s_id AND s.dp_id = dp.dp_id AND s.sp_id = sp.sp_id AND s.w_id = w.w_id AND w.if_id = i.if_id AND k.k_data_kontroli > (sysdate - 7));
 select * from view_kontrole_week;
-drop view view_kontrole_week;
-
 
 create or replace view view_utarg (id_placowki, kod_pocztowy, miasto, ulica, nr, data, utarg_dzienny) 
 	as (SELECT DISTINCT pl.pl_id, ad.ad_kod_pocztowy, ad.ad_miasto, ad.ad_ulica, ad.ad_nr_domu, k.k_data_kontroli, SUM(k_cena) OVER(PARTITION BY k.k_data_kontroli)
 	FROM placowki pl, pracownicy p, kontrole k, adresy ad WHERE pl.ad_id = ad.ad_id AND pl.pl_id = p.pl_id 
 	AND p.p_id = k.p_id);
 select * from view_utarg;
-drop view view_utarg;
