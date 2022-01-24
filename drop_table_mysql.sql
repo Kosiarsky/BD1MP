6227b94d78fb57af1be6d078a0f9ded58423d775
Dawid Nowotny <93731073+ayaseshi@users.noreply.github.com>
2022-01-24 20:32:57 +0100

    Update Drop Table MySQL
    Aktualizacja zapytań usuwających tabele, widoki oraz procedury w MySQL.

--- drop_table_mysql.sql
+++ drop_table_mysql.sql
@@ -1,10 +1,15 @@
-drop view view_wlasciciele;
-drop view view_placowki;
-drop view view_pracownicy;
-drop view view_samochody;
-drop view view_kontrole;
-drop view view_kontrole_week;
-drop view view_utarg;
+DROP PROCEDURE IF EXISTS zmiana_stawki;
+DROP PROCEDURE IF EXISTS zmiana_premii;
+DROP PROCEDURE IF EXISTS zmiana_premii_st;
+DROP PROCEDURE IF EXISTS zmiana_stawki_st;
+DROP PROCEDURE IF EXISTS zmiana_ceny;
+drop view IF EXISTS view_wlasciciele;
+drop view IF EXISTS view_placowki;
+drop view IF EXISTS view_pracownicy;
+drop view IF EXISTS view_samochody;
+drop view IF EXISTS view_kontrole;
+drop view IF EXISTS view_kontrole_week;
+drop view IF EXISTS view_utarg;
 ALTER TABLE informacje_osobowe DROP FOREIGN KEY ifad_fk;
 ALTER TABLE kontrole DROP FOREIGN KEY kp_fk;
 ALTER TABLE kontrole DROP FOREIGN KEY ks_fk;
@@ -19,5 +24,4 @@ ALTER TABLE samochody DROP FOREIGN KEY sw_fk;
 ALTER TABLE wlasciciele DROP FOREIGN KEY wif_fk;	
 ALTER TABLE zmiany_pracownicze DROP FOREIGN KEY zpp_fk;	
 DROP TABLE IF EXISTS
-kontrole, pracownicy, samochody, placowki, zmiany_pracownicze, stanowiska, dane_pojazdow, stany_pojazdow, produkcja_pojazdow, wlasciciele, informacje_osobowe, adresy CASCADE;
-
+php_admin, kontrole, pracownicy, samochody, placowki, zmiany_pracownicze, stanowiska, dane_pojazdow, stany_pojazdow, produkcja_pojazdow, wlasciciele, informacje_osobowe, adresy CASCADE;
