42a980a971ea9a7ddd50c0b942a5e178b9a83060
Olaf152 <96248180+Olaf152@users.noreply.github.com>
2022-01-24 20:17:30 +0100

    Update Create Table Oracle
    Aktualizacja zapytań tworzących bazę danych w Oracle.

---
+++ create_table_oracle.sql
@@ -0,0 +1,212 @@
+CREATE TABLE adresy (
+    ad_id           NUMBER NOT NULL,
+    ad_kraj         VARCHAR2(30),
+    ad_wojewodztwo  VARCHAR2(30),
+    ad_miasto       VARCHAR2(30),
+    ad_ulica        VARCHAR2(30),
+    ad_nr_domu      NUMBER,
+    ad_kod_pocztowy VARCHAR2(6)
+);
+
+ALTER TABLE adresy ADD CONSTRAINT ad_pk PRIMARY KEY ( ad_id );
+
+CREATE TABLE dane_pojazdow (
+    dp_id                         NUMBER NOT NULL,
+    dp_paliwo                     VARCHAR2(11),
+    dp_przebieg                   NUMBER,
+    dp_pojemnosc                  VARCHAR2(3),
+    dp_moc_km                     NUMBER,
+    dp_moc_kw                     NUMBER,
+    dp_masa_wlasna                NUMBER,
+    dp_skrzynia_biegow            VARCHAR2(20)
+);
+
+ALTER TABLE dane_pojazdow ADD CONSTRAINT dp_pk PRIMARY KEY ( dp_id );
+
+CREATE TABLE informacje_osobowe (
+    if_id             NUMBER NOT NULL,
+    if_imie           VARCHAR2(15),
+    if_nazwisko       VARCHAR2(30),
+    if_plec           VARCHAR2(15),
+    if_data_urodzenia DATE,
+    if_pesel          VARCHAR2(11),
+    if_telefon        VARCHAR2(9),
+    ad_id             NUMBER NOT NULL
+);
+
+ALTER TABLE informacje_osobowe ADD CONSTRAINT if_pk PRIMARY KEY ( if_id );
+ALTER TABLE informacje_osobowe ADD CONSTRAINT if_unique UNIQUE ( if_pesel, if_telefon );
+ALTER TABLE informacje_osobowe ADD CONSTRAINT plec_check CHECK (if_plec IN ('Mezczyzna', 'Kobieta'));
+
+CREATE TABLE kontrole (
+    k_id                      NUMBER NOT NULL,
+    k_pozytywny               VARCHAR2(3),
+    k_lpg                     VARCHAR2(3),
+    k_hak                     VARCHAR2(3),
+    k_data_kontroli           DATE,
+    k_data_nastepnej_kontroli DATE,
+    k_cena                    NUMBER,
+    p_id                      NUMBER,
+    s_id                      NUMBER NOT NULL
+);
+
+ALTER TABLE kontrole ADD CONSTRAINT k_pk PRIMARY KEY ( k_id );
+ALTER TABLE kontrole ADD CONSTRAINT poz_check CHECK (k_pozytywny IN ('TAK', 'NIE'));
+ALTER TABLE kontrole ADD CONSTRAINT lpg_check CHECK (k_lpg IN ('TAK', 'NIE'));
+ALTER TABLE kontrole ADD CONSTRAINT hak_check CHECK (k_hak IN ('TAK', 'NIE'));
+
+CREATE TABLE placowki (
+    pl_id                 NUMBER NOT NULL,
+    pl_telefon            VARCHAR2(9),
+    pl_fax                VARCHAR2(12),
+    pl_godzina_otwarcia   VARCHAR2(5),
+    pl_godzina_zamkniecia VARCHAR2(5),
+    ad_id                 NUMBER NOT NULL
+);
+
+ALTER TABLE placowki ADD CONSTRAINT pl_pk PRIMARY KEY ( pl_id );
+ALTER TABLE placowki ADD CONSTRAINT ad_unique UNIQUE ( ad_id );
+
+CREATE TABLE pracownicy (
+    p_id                    NUMBER NOT NULL,
+    p_przepracowane_godziny NUMBER,
+    p_stawka                NUMBER,
+    pl_id                   NUMBER NOT NULL,
+    if_id                   NUMBER NOT NULL,
+    st_id                   NUMBER NOT NULL
+);
+
+ALTER TABLE pracownicy ADD CONSTRAINT p_pk PRIMARY KEY ( p_id );
+ALTER TABLE pracownicy ADD CONSTRAINT p_unique UNIQUE ( if_id, st_id );
+
+CREATE TABLE produkcja_pojazdow (
+    pp_id                         NUMBER NOT NULL,
+    pp_kraj_produkcji             VARCHAR2(30),
+    pp_data_produkcji             DATE,
+    pp_data_pierwszej_rejestracji DATE,
+    pp_data_rejestracji_w_kraju   DATE,
+    pp_ilosc_wlascicieli          NUMBER
+);
+
+ALTER TABLE produkcja_pojazdow ADD CONSTRAINT pp_pk PRIMARY KEY ( pp_id );
+
+CREATE TABLE samochody (
+    s_id                  NUMBER NOT NULL,
+    s_typ                 VARCHAR2(30),
+    s_numer_rejestracyjny VARCHAR2(30),
+    s_marka               VARCHAR2(30),
+    s_model               VARCHAR2(30),
+    s_generacja           VARCHAR2(30),
+    w_id                  NUMBER,
+    pp_id                 NUMBER NOT NULL,
+    dp_id                 NUMBER NOT NULL,
+    sp_id                 NUMBER NOT NULL
+);
+
+ALTER TABLE samochody ADD CONSTRAINT s_pk PRIMARY KEY ( s_id );
+ALTER TABLE samochody ADD CONSTRAINT s_unique UNIQUE ( s_numer_rejestracyjny, pp_id, dp_id, sp_id );
+
+CREATE TABLE stanowiska (
+    st_id             NUMBER NOT NULL,
+    st_nazwa          VARCHAR2(30),
+    st_opis           VARCHAR2(200),
+    st_premia         NUMBER,
+    st_data_uzyskania DATE
+);
+
+ALTER TABLE stanowiska ADD CONSTRAINT st_pk PRIMARY KEY ( st_id );
+
+CREATE TABLE stany_pojazdow (
+    sp_id           NUMBER NOT NULL,
+    sp_silnik       VARCHAR2(30),
+    sp_hamulce      VARCHAR2(30),
+    sp_amortyzatory VARCHAR2(30),
+    sp_zawieszenie  VARCHAR2(30),
+    sp_zarowki      VARCHAR2(30),
+    sp_lampy        VARCHAR2(30)
+);
+
+ALTER TABLE stany_pojazdow ADD CONSTRAINT sp_pk PRIMARY KEY ( sp_id );
+ALTER TABLE stany_pojazdow ADD CONSTRAINT silnik_check CHECK (sp_silnik IN ('SPRAWNE', 'NIESPRAWNE'));
+ALTER TABLE stany_pojazdow ADD CONSTRAINT hamulce_check CHECK (sp_hamulce IN ('SPRAWNE', 'NIESPRAWNE'));
+ALTER TABLE stany_pojazdow ADD CONSTRAINT amortyzatory_check CHECK (sp_amortyzatory IN ('SPRAWNE', 'NIESPRAWNE'));
+ALTER TABLE stany_pojazdow ADD CONSTRAINT zawieszenie_check CHECK (sp_zawieszenie IN ('SPRAWNE', 'NIESPRAWNE'));
+ALTER TABLE stany_pojazdow ADD CONSTRAINT zarowki_check CHECK (sp_zarowki IN ('SPRAWNE', 'NIESPRAWNE'));
+ALTER TABLE stany_pojazdow ADD CONSTRAINT lampy_check CHECK (sp_lampy IN ('SPRAWNE', 'NIESPRAWNE'));
+
+CREATE TABLE wlasciciele (
+    w_id                  NUMBER NOT NULL,
+    w_data_prawa_jazdy    DATE,
+    w_waznosc_prawa_jazdy DATE,
+    w_ilosc_samochodow    NUMBER,
+    w_procent_znizek_oc   NUMBER,
+    if_id                 NUMBER NOT NULL
+);
+
+ALTER TABLE wlasciciele ADD CONSTRAINT w_pk PRIMARY KEY ( w_id );
+ALTER TABLE wlasciciele ADD CONSTRAINT w_unique UNIQUE ( if_id );
+
+CREATE TABLE zmiany_pracownicze (
+    zp_id                  NUMBER NOT NULL,
+    zp_data                DATE,
+    zp_godzina_rozpoczecia VARCHAR2(10),
+    zp_godzina_zakonczenia VARCHAR2(10),
+    zp_liczba_kontroli     NUMBER,
+    zp_przerwa             VARCHAR2(3),
+    p_id                   NUMBER NOT NULL
+);
+
+ALTER TABLE zmiany_pracownicze ADD CONSTRAINT zp_pk PRIMARY KEY ( zp_id );
+ALTER TABLE zmiany_pracownicze ADD CONSTRAINT przerwa_check CHECK (zp_przerwa IN ('TAK', 'NIE'));
+
+ALTER TABLE informacje_osobowe
+    ADD CONSTRAINT ifad_fk FOREIGN KEY ( ad_id )
+        REFERENCES adresy ( ad_id );
+
+ALTER TABLE kontrole
+    ADD CONSTRAINT kp_fk FOREIGN KEY ( p_id )
+        REFERENCES pracownicy ( p_id );
+
+ALTER TABLE kontrole
+    ADD CONSTRAINT ks_fk FOREIGN KEY ( s_id )
+        REFERENCES samochody ( s_id );
+
+ALTER TABLE placowki
+    ADD CONSTRAINT pa_fk FOREIGN KEY ( ad_id )
+        REFERENCES adresy ( ad_id );
+
+ALTER TABLE pracownicy
+    ADD CONSTRAINT pif_fk FOREIGN KEY ( if_id )
+        REFERENCES informacje_osobowe ( if_id );
+
+ALTER TABLE pracownicy
+    ADD CONSTRAINT ppl_fk FOREIGN KEY ( pl_id )
+        REFERENCES placowki ( pl_id );
+
+ALTER TABLE pracownicy
+    ADD CONSTRAINT pst_fk FOREIGN KEY ( st_id )
+        REFERENCES stanowiska ( st_id );
+
+ALTER TABLE samochody
+    ADD CONSTRAINT sdp_fk FOREIGN KEY ( dp_id )
+        REFERENCES dane_pojazdow ( dp_id );
+
+ALTER TABLE samochody
+    ADD CONSTRAINT spp_fk FOREIGN KEY ( pp_id )
+        REFERENCES produkcja_pojazdow ( pp_id );
+
+ALTER TABLE samochody
+    ADD CONSTRAINT ssp_fk FOREIGN KEY ( sp_id )
+        REFERENCES stany_pojazdow ( sp_id );
+
+ALTER TABLE samochody
+    ADD CONSTRAINT sw_fk FOREIGN KEY ( w_id )
+        REFERENCES wlasciciele ( w_id );
+
+ALTER TABLE wlasciciele
+    ADD CONSTRAINT wif_fk FOREIGN KEY ( if_id )
+        REFERENCES informacje_osobowe ( if_id );
+
+ALTER TABLE zmiany_pracownicze
+    ADD CONSTRAINT zpp_fk FOREIGN KEY ( p_id )
+        REFERENCES pracownicy ( p_id ); 
